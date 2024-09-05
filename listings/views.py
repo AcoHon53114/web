ca95b0b3e785240d5e956c43196717ab2616353f
acohon <showwinho@gmail.com>
2024-09-05 13:17:56 +0800

    Step15: add listing pages  content

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing
 # Create your views here.
 
 def index(request):
     #return HttpResponse("<h1>Hello World</h1>")
-    return render(request, 'listings/listings.html')
+    
+    # ! get all data from listing database 
+    listings = Listing.objects.all()
+    # ! pass database records into listings context
+    context ={'listings':listings}
+    
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
