7d7b39a4a40703c8f8ff48fc12d51f9b985eb647
acohon <showwinho@gmail.com>
2024-09-06 12:35:07 +0800

    Step16: modified release day value of istings page

--- realtors/views.py
+++ realtors/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Realtor
 # Create your views here.
+
+def about(request):
+    # ! get all data from listing database 
+    realtors = Realtor.objects.all()
+    # ! pass database records into listings context
+    context ={'realtors':realtors}
+    
+    return render(request, 'pages/about.html', context)
