1cd78ad7fe4c0ac40e256537e8ac9af099a84cd4
acohon <showwinho@gmail.com>
2024-09-10 15:32:30 +0800

    Step22: add search page basic function

--- bcre/settings.py
+++ bcre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7=8#)fcp=-=q5b(#z@-c_a+%p)7e5l^iwf7+$n6s!7fjsdlp3o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1']
--- listings/views.py
+++ listings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Listing
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
-
+from listings.choices import price_choices,bedroom_choices,district_choices
 
 # Create your views here.
 
@@ -28,4 +28,11 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'district_choices': district_choices,
+        'listings': queryset_list,
+    }
+    return render(request, 'listings/search.html', context, )
