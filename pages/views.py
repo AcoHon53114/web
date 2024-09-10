02ab124a3c8a57e42ca6693d07c6d35c1d98e79b
acohon <showwinho@gmail.com>
2024-09-10 12:49:19 +0800

    Step21: add search function in index page

--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices,bedroom_choices,district_choices
 # Create your views here.
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    context ={'listings': listings}
+    context ={'listings': listings,
+              'price_choices': price_choices,
+              'bedroom_choices': bedroom_choices,
+              'district_choices': district_choices}
     #return HttpResponse("<h1>Hello World</h1>")
     return render(request, 'pages/index.html', context)
 
